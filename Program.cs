9b86f68b0bed4d8da145368e0e5bb795537db03e
vfursov86 <vfursov86@gmail.com>
2025-02-23 17:39:33 +0300

    Program.cs completed and application environment files.

--- Program.cs
+++ Program.cs
@@ -1,41 +1,31 @@
+using Prometheus;
+using Microsoft.Extensions.Logging;
+using static Microsoft.Extensions.Logging.Log4NetExtensions;
+//--------------------------------------------------------------
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Disable standard logger provider.
+builder.Logging.ClearProviders();
+builder.Logging.AddLog4Net();
+
+// See more Kestrel server options https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel/endpoints?view=aspnetcore-9.0
+builder.WebHost.ConfigureKestrel(options =>
+{
+    serverOptions.Limits.KeepAliveTimeout = TimeSpan.Seconds(3);
+    serverOptions.Limits.MaxConcurrentConnections = 200;
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseResponseCaching();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Add Prometheus exporter.
+app.UseMetricServer();
+app.UseHttpMetrics();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # qpssbpmock
+
+## Description
+Implements a response logic from Fast Transfers service.
+Project based on ASP.NET Core Web API template
+and exposes API with three Controllers.
+In general, this is an API server with advanced logging 
+and many of performance build-in metrics. 
+Viktor Fursov. 2024.
+
+This mock exposes the following endpoints (grouped by Controllers):
+
+1. TransfersController
+    - /c2c/transfers/init
+    - /c2c/transfers/confirm
+    - /c2c/transfers/CheckReceiver
+    - /c2c/transfers/participantList
+2. IntegratioController
+    - /c2c/integration/CheckReceiver
+3. FtsController
+    - /c2c/fts/banklist
+    - / 
+
+## Rinning options
+While local deploying you use the launchSettins.json, and appsettings.Development.json configuration files.
+In production deployment you using appsettings.json.
+If URL and port are not provided, the application runs at localhost:5000.
+
+## Logging
+Default logging is disabled and log4net used instead.
+
+## Response time
+You set the mock's response time with "MockResponseTime" parameter in appsettings.json
+ans application.Development.json.
+
+## Metrics
+Full of necessary performance metrics provided by Prometheus exporter,
+and available at the /metrics endpoint.
